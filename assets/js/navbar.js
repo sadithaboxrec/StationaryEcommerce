7eb1f55e449b191705e486bf4bf3a4f819007540
Saditha Fernando <sadithafernandods2002@gmail.com>
2025-08-09 23:07:45 +0530

    Navbar

---
+++ assets/js/navbar.js
@@ -0,0 +1,18 @@
+const activePage = window.location.pathname.split("/").pop();
+
+document.querySelectorAll('.navbar-menu a').forEach(link => {
+    const linkPage = link.getAttribute("href");
+    if (linkPage === activePage) {
+        link.classList.add("active");
+    }
+});
+
+
+
+const navbarToggle = document.querySelector('.navbar-toggle');
+const navbarMenu = document.querySelector('.navbar-menu');
+
+navbarToggle.addEventListener('click', () => {
+    navbarToggle.classList.toggle('active');
+    navbarMenu.classList.toggle('active');
+});
---
+++ includes/navbar.php
@@ -0,0 +1,17 @@
+<nav class="navbar">
+    <div class="navbar-container">
+        <a href="index.html" class="navbar-logo">Stationary Web</a>
+        <button class="navbar-toggle">
+            <span class="bar"></span>
+            <span class="bar"></span>
+            <span class="bar"></span>
+        </button>
+        <ul class="navbar-menu">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Page1</a></li>
+            <li><a href="#">Page2</a></li>
+            <li><a href="#">Page3</a></li>
+            <li><a href="#">Page4</a></li>
+        </ul>
+    </div>
+</nav>
