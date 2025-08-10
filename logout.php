6d1499310e9a62694782b4739fc2936a2ae43ba0
Saditha Fernando <sadithafernandods2002@gmail.com>
2025-08-10 17:40:08 +0530

    Login and Logout of User

---
+++ logout.php
@@ -0,0 +1,13 @@
+<?php
+
+if(isset($_SESSION['authentication'])){
+
+    unset($_SESSION['authentication']);
+    unset($_SESSION['authentication_user']);
+
+    $_SESSION['message'] = "Logged Out Successfully";
+}
+
+header('Location:index.php');
+
+?>
