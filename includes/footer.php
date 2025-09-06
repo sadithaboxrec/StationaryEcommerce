9ba724e9c5267d66038d9ab1396d3ca8c392c74d
Saditha Fernando <sadithafernandods2002@gmail.com>
2025-09-06 20:12:17 +0530

    navbar change

--- includes/footer.php
+++ includes/footer.php
@@ -6,6 +6,11 @@
         integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo="
         crossorigin="anonymous"></script>
 
+        
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
 
 <script src="assets/js/navbar.js"></script>
 </body>
--- includes/navbar.php
+++ includes/navbar.php
@@ -1,31 +1,142 @@
-<nav class="navbar">
-    <div class="navbar-container">
-        <a href="#" class="navbar-logo">Stationary Web</a>
-        <button class="navbar-toggle">
-            <span class="bar"></span>
-            <span class="bar"></span>
-            <span class="bar"></span>
-        </button>
-        <ul class="navbar-menu">
-            <li><a href="#">Home</a></li>
-            <li><a href="#">Page1</a></li>
-            <li><a href="#">Page2</a></li>
-            <li><a href="#">Page3</a></li>
+<header>
+  
+  <div class="p-3 text-center bg-white border-bottom">
+    <div class="container-fluid">
+      <div class="row align-items-center">
+        <div class="col-md-4 text-center text-md-left mb-3 mb-md-0">
+          <a href="#!">
+            <img src="https://via.placeholder.com/150x35?text=StationaryMart" height="35" alt="stationaryMart"/>
+          </a>
+        </div>
 
-            <?php
-                      if(isset($_SESSION['authentication'])){
-            ?>
-                    <li><?= $_SESSION['authentication_user']['name']; ?></li>
-                    <li><a href="logout.php">Log Out</a></li>
+        <div class="col-md-4">
+          <form class="d-flex">
+            <input type="search" class="form-control" placeholder="Search" />
+            <button class="btn btn-outline-secondary ml-2" type="submit">
+              <i class="fa fa-search"></i>
+            </button>
+          </form>
+        </div>
 
-            <?php
-            }else{
-            ?>
-                        <li><a href="login.php">Login</a></li>
-            <?php
-            }
-            ?>
+        <div class="col-md-4 text-center text-md-right">
+          <a href="#" class="text-dark mr-3">
+            <i class="fa fa-shopping-cart"></i>
+            <span class="badge badge-danger">1</span>
+          </a>
 
-        </ul>
+          
+                <?php if (isset($_SESSION['authentication'])): ?>
+                <!-- Logged-in: Show avatar and dropdown -->
+                <div class="btn-group">
+                    <button type="button" class="btn btn-link dropdown-toggle p-0" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="border: none;">
+                    <img src="https://mdbootstrap.com/img/Photos/Avatars/img (31).jpg"
+                        class="rounded-circle"
+                        height="35"
+                        alt="User Avatar"
+                        style="cursor: pointer; border: 2px solid #28a745; padding: 2px;"
+                    />
+                    </button>
+                    <div class="dropdown-menu dropdown-menu-right">
+                    <span class="dropdown-item-text font-weight-bold text-success">
+                        <?= htmlspecialchars($_SESSION['authentication_user']['name']); ?>
+                    </span>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="#">My Profile</a>
+                    <a class="dropdown-item" href="logout.php">Logout</a>
+                    </div>
+                </div>
+                <?php else: ?>
+                <!-- Not logged-in: Show 'Login' styled -->
+                <a href="login.php" class="btn btn-success btn-sm ml-2 font-weight-bold">
+                    <i class="fa fa-sign-in mr-1"></i> Login
+                </a>
+                <?php endif; ?>
+
+
+
+        </div>
+      </div>
     </div>
-</nav>
+  </div>
+
+
+  <nav class="navbar navbar-expand-lg navbar-light border-bottom">
+    <div class="container-fluid">
+      <a class="nav-link" href="#">Home</a>
+
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#mainNavbar">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="mainNavbar">
+        <ul class="navbar-nav mr-auto">
+          
+          <li class="nav-item dropdown position-static">
+            <a class="nav-link dropdown-toggle" href="#" id="categoriesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Categories
+            </a>
+            <div class="dropdown-menu w-100 mt-0 border-0" aria-labelledby="categoriesDropdown">
+              <div class="container py-4">
+                <div class="row">
+                 
+                  <div class="col-md-4">
+                    <h6 class="text-uppercase">Explicabo Voluptas</h6>
+                    <a href="#" class="text-dark">
+                      <div class="media mb-3">
+                        <img src="https://mdbootstrap.com/img/new/standard/city/041.jpg" class="mr-3 img-fluid rounded" style="width: 60px;" alt="">
+                        <div class="media-body">
+                          <p class="mb-1"><strong>Lorem ipsum</strong></p>
+                          <small class="text-muted">15.07.2020</small>
+                        </div>
+                      </div>
+                    </a>
+                  </div>
+                  
+                  <div class="col-md-4">
+                    <h6 class="text-uppercase">Iste Quaerato</h6>
+                    <a href="#" class="text-dark">
+                      <div class="media mb-3">
+                        <img src="https://mdbootstrap.com/img/new/standard/city/043.jpg" class="mr-3 img-fluid rounded" style="width: 60px;" alt="">
+                        <div class="media-body">
+                          <p class="mb-1"><strong>Lorem ipsum</strong></p>
+                          <small class="text-muted">15.07.2020</small>
+                        </div>
+                      </div>
+                    </a>
+                  </div>
+                
+                  <div class="col-md-4">
+                    <h6 class="text-uppercase">Cras Justo Odio</h6>
+                    <a href="#" class="text-dark">
+                      <div class="media mb-3">
+                        <img src="https://mdbootstrap.com/img/new/standard/city/045.jpg" class="mr-3 img-fluid rounded" style="width: 60px;" alt="">
+                        <div class="media-body">
+                          <p class="mb-1"><strong>Lorem ipsum</strong></p>
+                          <small class="text-muted">15.07.2020</small>
+                        </div>
+                      </div>
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </li>
+
+         
+          <li class="nav-item">
+            <a class="nav-link" href="#">Products</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">About Us</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Contact Us</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">FAQ</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</header>
