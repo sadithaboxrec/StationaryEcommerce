03de943288fbd33ffe14aca691cbef8317ed4782
Saditha Fernando <sadithafernandods2002@gmail.com>
2025-09-09 16:40:37 +0530

    Display Added Cart items

---
+++ cart.php
@@ -0,0 +1,62 @@
+<?php 
+include('functions/frontendfunctions.php');
+include('includes/header.php');
+?>
+<h3 class="mb-3 mt-2 pb-1 pb-md-0 mb-md-5 text-center">Cart Items</h3>
+        <div class="container my-5">
+            <div class="card shadow">
+                <div class="card-header bg-primary text-white">
+                    
+                </div>
+                <div class="card-body p-0">
+                    <div class="table-responsive">
+                        <table class="table table-hover align-middle mb-0">
+                            <tbody>
+                                <?php  
+                                    $cartitems = getCartItems();
+
+                                    if(mysqli_num_rows($cartitems) > 0){
+                                        foreach($cartitems as $item){
+                                ?>
+                                    <tr>
+                                        
+                                        <td>
+                                            <img src="uploads/products/<?= $item['image'] ?>" alt="Product Image" class="rounded-circle" width="50" height="50">
+                                        </td>
+
+                                        <td><?= htmlspecialchars($item['name']); ?></td>
+
+                                        <td><?= htmlspecialchars($item['selling_price']); ?></td>
+
+                                        <td>
+                                            <input type="number" class="qty-input" value="<?php echo $item['quantity']; ?>" min="1" max="10">
+                                        </td>
+
+
+                                        <td>
+                                            <button type="submit" name="deleteProductBtn" class="btn btn-sm btn-outline-danger">
+                                                <i class="fa fa-trash"></i>
+                                                Remove
+                                            </button>
+                                        </td>
+                                    </tr>
+                                <?php
+                                        }
+                                    } else {
+                                ?>
+                                    <tr>
+                                        <td colspan="5" class="text-center text-muted py-4">No data found</td>
+                                    </tr>
+                                <?php
+                                    }
+                                ?>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+<?php include('includes/footer.php'); ?>
--- functions/frontendfunctions.php
+++ functions/frontendfunctions.php
@@ -28,6 +28,30 @@ function getActiveProductsByCategory($category_id){
 
 }
 
+function getCartItems(){
+
+    global $connection;
+
+    $user_id = $_SESSION['authentication_user']['user_id'];
+    $retrieve_cart_items_query = "
+                                        SELECT 
+                                            c.id AS cid, 
+                                            c.product_id, 
+                                            c.quantity, 
+                                            p.id AS pid, 
+                                            p.name, 
+                                            p.image, 
+                                            p.original_price, 
+                                            p.selling_price 
+                                        FROM carts c
+                                        JOIN products p ON c.product_id = p.id
+                                        WHERE c.user_id = '$user_id'
+                                        ORDER BY c.id DESC"      ;
+
+    return   $retrieve_cart_items_query_sql=mysqli_query($connection,$retrieve_cart_items_query);
+
+}
+
 function redirect($url,$message){
     
     $_SESSION['message']=$message;
